0dede2c55d39cdad4c0b2ca402e7a985a92d3edf
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-08 17:24:42 +0530

    Added backend validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
+  validates :email, presence: true, uniqueness: true
+  validates :password, format: {with: /[!@#$%^&*(),.?":{}|<>]/, message: "must include at least one special character"}, length: { minimum:6 } 
 end
