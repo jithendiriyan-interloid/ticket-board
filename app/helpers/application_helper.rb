23a2d52761fc98fa6e9f7a62d98e0d87e6bff781
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-09 14:45:33 +0530

    Implemented Navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def hide_navbar?
-    ![ "sessions", "registrations", "passwords", "confirmations", "unlocks" ].include?(controller_name)
-  end
 end
---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,18 @@
+import {Controller} from "@hotwired/stimulus"
+export default class extends Controller {
+    static targets=["menu","button"]
+    connect() {
+        this.handleOutsideClick=this.hide.bind(this)
+        document.addEventListener("click",this.handleOutsideClick)
+    }
+    disconnect() {
+        document.removeEventListener("click",this.handleOutsideClick)
+    }
+    toggle(event) {
+        event.stopPropagation() 
+        this.menuTarget.classList.toggle("hidden")
+    }
+    hide() {
+        this.menuTarget.classList.add("hidden")
+    }
+}
