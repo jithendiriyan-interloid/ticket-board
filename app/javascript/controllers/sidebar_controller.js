270f36dfd1c08df4ef191721c477a9ddac85f3d8
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-14 14:55:00 +0530

    sidebar toggle implemented

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,17 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["panel","icon"]
-
-  open() {
-    this.panelTarget.classList.remove("-translate-x-[220px]")
-    this.iconTarget.classList.remove("rotate-180")
-  }
-
+  static targets = ["panel","icon","toggle"]
   toggle() {
-    this.panelTarget.classList.toggle("-translate-x-[220px]")
+    this.panelTarget.classList.toggle("-translate-x-full")
     this.iconTarget.classList.toggle("rotate-180")
-
+    this.toggleTarget.classList.toggle("-right-10")
   }
 }
 
