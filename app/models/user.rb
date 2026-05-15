41055136bac6a54935df7bd5247f027c5afc6001
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-15 20:20:48 +0530

    Implemented UserProfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
   validates :email, presence: true, uniqueness: true
-  validates :password, format: {with: /[!@#$%^&*(),.?":{}|<>]/, message: "must include at least one special character"}, length: { minimum:6 } 
+  validates :password, length: { minimum: 6 },  format: { with: /[!@#$%^&*(),.?":{}|<>]/, message: "must include at least one special character" }, if: -> { password.present? }
 end
