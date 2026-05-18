195d62b01d481d4afb1ff2e382710f7577cef293
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-18 20:08:49 +0530

    Pundit authorization function added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
   def edit
     @user = current_user
+    authorize @user
+    if current_user.owner?
+        @users = User.all
+    end
   end
 
   def show
     @user = current_user
+    authorize @user
   end
 
   def destroy
+    @user = current_user
+    authorize @user
     if current_user.soft_delete!
       sign_out(current_user)
       redirect_to root_path, notice: "Account was deleted successfully"
@@ -17,11 +24,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = current_user
+    @user = User.find(params[:id])
+    authorize @user
     if @user.update(profile_params)
-      redirect_to root_path, notice: "Profile updated successfully"
+      redirect_back(
+        fallback_location: root_path,
+        notice: "User updated successfully"
+      )
     else
-      render :edit, status: :unprocessable_entity
+      redirect_back(
+        fallback_location: root_path,
+        alert: "Unable to update user"
+      )
     end
   end
 
