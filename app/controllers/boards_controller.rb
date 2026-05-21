04bb28f3f0d249bd919753bb486c9876cabb224c
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-21 12:37:22 +0530

    Update Status bar

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,5 @@ class BoardsController < ApplicationController
   def index
     @user = current_user 
     @statuses = Status.includes(:tasks)
-      @new_status_form = params[:new_status].present?
   end
 end
--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,21 @@
 class StatusController < ApplicationController
+  def new
+    @status = Status.new
+  end
+
   def create
-    Status.create!(name: params[:name])
-    redirect_to board_path
+    @status = Status.new(status_params)
+
+    if @status.save
+      redirect_to boards_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:name)
   end
 end
