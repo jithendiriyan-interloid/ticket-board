662d1831fb9478cb679dccf57964c5899192dbff
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-22 10:41:14 +0530

    Implemented sidebar

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["panel","icon","toggle"]
+  toggle() {
+    this.panelTarget.classList.toggle("-translate-x-full")
+    this.iconTarget.classList.toggle("rotate-180")
+    this.toggleTarget.classList.toggle("-right-10")
+  }
+}
