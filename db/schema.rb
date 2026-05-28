0cee948718da4fdccf12be61f2f10c88f01a96b4
jithendiriyan-interloid <jithendiriyan@interloid.com>
2026-05-28 10:09:42 +0530

    Active Storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_27_140634) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_28_043809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -189,6 +189,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_140634) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "avatar"
     t.string "city"
     t.datetime "confirmation_sent_at"
     t.string "confirmation_token"
